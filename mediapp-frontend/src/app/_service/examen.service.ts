5bfae82404181e4589ffdedf09d9268808062324
juan <68668799+JuanCeron023@users.noreply.github.com>
2022-07-01 19:14:51 -0500

    Update examen.service.ts

--- mediapp-frontend/src/app/_service/examen.service.ts
+++ mediapp-frontend/src/app/_service/examen.service.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ExamenService {
-
+ 
   examenCambio = new Subject<Examen[]>();
   mensajeCambio = new Subject<string>();
 
